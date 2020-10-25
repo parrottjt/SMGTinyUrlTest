dbc1908671e756b98a0c0568fe0715033ca5adcd
parrottjt <47727512+parrottjt@users.noreply.github.com>
2020-10-25 08:07:46 -0400

    Uploadeing to new repository

---
+++ SMG Mobile Test/TinyURLTest/TinyURL.Web/Models/ImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using TinyURL.Data.Models;
+
+namespace TinyURL.Web.Models
+{
+    public class ImageViewModel
+    {
+        //Labeling the content to be a upload, to ensure uploading
+        [DataType(DataType.Upload)]
+        [Display(Name = "Image File")]
+        [Required(ErrorMessage = "Please Choose A File To Upload.")]
+        public string file { get; set; }
+
+        public UploadedImage Image { get; set; }
+    }
+}
